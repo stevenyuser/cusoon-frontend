ba865518155110c031421a599575b02e238b3563
Steven Yu <stevenyuser@gmail.com>
2024-01-10 23:41:23 -0500

    fixed dark mode bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
     <footer className="bg-gray-200 text-center lg:text-left">
-        <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
+        <div className="p-4 text-center text-neutral-700">
             Made with ❤️ and 🧠 by <span className="font-medium">@stevenyuser</span> and <span className="font-medium">@jasminexli</span>
         </div>
     </footer>
